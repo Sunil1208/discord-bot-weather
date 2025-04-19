47a815164635679cfbc8396765cb42520be7f8f0
sunil-kumar <skmy03@gmail.com>
2025-04-20 00:10:50 +0530

    Added error handling

--- src/events/interactionCreateHandler.js
+++ src/events/interactionCreateHandler.js
@@ -1,7 +1,36 @@
 const interactionCreateHandler = async (interaction) => {
+  if (!interaction.isChatInputCommand()) {
+    return;
+  }
+
   const command = interaction.client.commands.get(interaction.commandName);
 
-  await command.execute(interaction);
+  if (!command) {
+    console.error(`No command matching ${interaction.commandName} was found.`);
+    return;
+  }
+
+  try {
+    await command.execute(interaction);
+
+    console.log(
+      `${interaction.user.username} executed the command ${interaction.commandName} in ${interaction.channel.name}`
+    );
+  } catch (error) {
+    console.error(error);
+    // if the interaction is replied or it is deferred, i.e. took too long
+    if (interaction.replied || interaction.deferred) {
+      await interaction.followUp({
+        content: "An error occurred while executing that command.",
+        ephemeral: true,
+      });
+    } else {
+      await interaction.reply({
+        content: "An error occurred while executing that command.",
+        ephemeral: true,
+      });
+    }
+  }
 };
 
 module.exports = {
